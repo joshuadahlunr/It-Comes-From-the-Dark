785b35981814f03041505b86c68763821f185c85
Joshua Dahl <joshuadahl@nevada.unr.edu>
2021-04-18 15:28:07 -0700

    Added Collisions to Player
    Adjusted the player controller scripts to now depend on a rigid body and collider. Added a test scene so that the player can be tested. Fixed a bug with the camera script through which you could look up so far you wound up flipping the camera upside down.

--- Assets/CharacterControl.cs
+++ Assets/CharacterControl.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class CharacterControl : MonoBehaviour
 {
-    CharacterController characterController;
+    CharacterController characterController; // Why is this needed?
+    public Rigidbody collision;
     public float speed = 0.5f;
     private float translation;
     private float strafe;
@@ -12,7 +13,8 @@ public class CharacterControl : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        characterController = GetComponent<CharacterController>();
+        characterController = GetComponent<CharacterController>(); // Why is this needed?
+        collision = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -20,6 +22,8 @@ public class CharacterControl : MonoBehaviour
     {
         translation = Input.GetAxis("Vertical") * speed * Time.deltaTime;
         strafe = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-        transform.Translate(strafe, 0, translation);
+
+        //transform.Translate(strafe, 0, translation);
+        collision.MovePosition(transform.position + transform.forward * translation + transform.right * strafe);
     }
 }
--- Assets/MouseCam.cs
+++ Assets/MouseCam.cs
@@ -4,18 +4,18 @@ using UnityEngine;
 
 public class MouseCam : MonoBehaviour
 {
-    [SerializeField]
+    [SerializeField] // Why do the fields need to be seralized whent they are public intrinsic types?
     public float sensitivity = 5.0f;
     [SerializeField]
     public float smoothing = 2.0f;
-    public GameObject character;
+    public Rigidbody character;
     private Vector2 mouseLook;
     private Vector2 smoothV;
 
     // Start is called before the first frame update
     void Start()
     {
-        character = this.transform.parent.gameObject;
+        character = this.transform.parent.gameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -27,7 +27,11 @@ public class MouseCam : MonoBehaviour
         smoothV.y = Mathf.Lerp(smoothV.y, mouseDelta.y, 1f / smoothing);
         mouseLook += smoothV;
 
+        // Lock the camera so that you can't look down too far, and you can't look up to far (fixes issue with being able to look up so far the camera becomes upside down!)
+        mouseLook.y = Mathf.Clamp(mouseLook.y, -50, 90);
+
         transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
-        character.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, character.transform.up);
+        character.MoveRotation(Quaternion.AngleAxis(mouseLook.x, character.transform.up));
+        //character.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, character.transform.up);
     }
 }
