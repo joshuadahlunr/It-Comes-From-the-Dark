4c9f4d451cea6237ba69bb179afb979543a34993
LegendaryCorn <43460114+LegendaryCorn@users.noreply.github.com>
2021-04-18 19:46:53 -0700

    Main Menu
    Added a main menu.

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Button startGameButton;
+    
+    
+    void Start()
+    {
+        startGameButton.onClick.AddListener(OnClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+            
+    }
+
+    void OnClick()
+    {
+        SceneManager.LoadScene("Player Test", LoadSceneMode.Single);
+    }
+}
