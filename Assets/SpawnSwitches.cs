fbc9b83b9a2dda520eb3f7af5e592e5ac41fe16e
mdesear <82544367+mdesear@users.noreply.github.com>
2021-04-25 14:25:44 -0700

    Added switch generation
    Game should iterate through array of potential spawn points and spawn x number of light switches in random points. Still need to implement the actual lights and switch functionality.

---
+++ Assets/SpawnSwitches.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnSwitches : MonoBehaviour
+{
+    public Transform[] SpawnPoints;
+    public int numSwitches = 5;
+    public GameObject Switches;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < numSwitches; i++)
+        {
+            Spawn();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Spawn()
+    {
+        int spawnIndex = Random.Range(0, SpawnPoints.Length); // generate random array number
+        Instantiate(Switches, SpawnPoints[spawnIndex].position, SpawnPoints[spawnIndex].rotation);
+    }
+}
