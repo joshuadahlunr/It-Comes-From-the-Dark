b63418ffafb06f06d720758d66d881629511f1cf
mdesear <82544367+mdesear@users.noreply.github.com>
2021-04-25 18:02:23 -0700

    Switch spawn and light activation
    switch random generation and player should be able to turn the spawned lights on and off when within range. still need to do lights turning off after x amount of time.

--- Assets/SpawnSwitches.cs
+++ Assets/SpawnSwitches.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class SpawnSwitches : MonoBehaviour
 {
-    public Transform[] SpawnPoints;
-    public int numSwitches = 5;
+    public GameObject[] SpawnPoints;
     public GameObject Switches;
+
+    public Light[] lights;
+
+    public int numSwitches = 5;
+    public float timeOn = 30;
+
+    private bool on;
+
+    SphereCollider switchCollider;
 
     // Start is called before the first frame update
     void Start()
@@ -20,12 +28,25 @@ public class SpawnSwitches : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        LightLogic();
     }
 
     void Spawn()
     {
         int spawnIndex = Random.Range(0, SpawnPoints.Length); // generate random array number
-        Instantiate(Switches, SpawnPoints[spawnIndex].position, SpawnPoints[spawnIndex].rotation);
+        Instantiate(Switches, SpawnPoints[spawnIndex].transform.position, SpawnPoints[spawnIndex].transform.rotation);
+        switchCollider = SpawnPoints[spawnIndex].GetComponent<SphereCollider>(); // get appropriate collider
+        switchCollider.radius = 1; // make the collider larger so player can interact
+
+        if (switchCollider.tag == "Player" && Input.GetKeyDown(KeyCode.E)) // should be able to activate switch if in range
+        {
+            on = !on;
+            lights[spawnIndex].enabled = on;
+        }
+    }
+
+    void LightLogic()
+    {
+        // to do later, after certain amt of time has passed turn off light
     }
 }
